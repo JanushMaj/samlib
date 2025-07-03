f8e8a07ace41acc9f18cd81fabd3490574c03815
JanushMaj <35202635+JanushMaj@users.noreply.github.com>
2025-07-03 19:09:46 +0200

    refactor grafik models to use dtos

--- domain/models/grafik/impl/delivery_planning_element.dart
+++ domain/models/grafik/impl/delivery_planning_element.dart
@@ -1,4 +1,3 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import '../enums.dart';
 import '../grafik_element.dart';
 
@@ -28,42 +27,4 @@ class DeliveryPlanningElement extends GrafikElement {
     closed: closed,
   );
 
-  @override
-  Map<String, dynamic> toJson() {
-    final base = baseToJson();
-    return {
-      ...base,
-      'orderId': orderId,
-      'category': category.toString(),
-    };
-  }
-
-  factory DeliveryPlanningElement.fromJson(Map<String, dynamic> json) {
-    final id = (json['id'] as String?) ?? '';
-    final startTs = json['startDateTime'] as Timestamp?;
-    final endTs = json['endDateTime'] as Timestamp?;
-    final additionalInfo = (json['additionalInfo'] as String?) ?? '';
-    final addedByUserId = (json['addedByUserId'] as String?) ?? '';
-    final addedTimestamp = (json['addedTimestamp'] as Timestamp?)?.toDate() ?? DateTime(1960, 2, 9);
-    final closed = (json['closed'] as bool?) ?? false;
-
-    final orderId = (json['orderId'] as String?) ?? '';
-    final categoryStr = (json['category'] as String?) ?? 'DeliveryPlanningCategory.Inne';
-    final category = DeliveryPlanningCategory.values.firstWhere(
-          (e) => e.toString() == categoryStr,
-      orElse: () => DeliveryPlanningCategory.Inne,
-    );
-
-    return DeliveryPlanningElement(
-      id: id,
-      startDateTime: startTs?.toDate() ?? DateTime.now(),
-      endDateTime: endTs?.toDate() ?? DateTime.now(),
-      additionalInfo: additionalInfo,
-      orderId: orderId,
-      category: category,
-      addedByUserId: addedByUserId,
-      addedTimestamp: addedTimestamp,
-      closed: closed,
-    );
-  }
 }
--- domain/models/grafik/impl/time_issue_element.dart
+++ domain/models/grafik/impl/time_issue_element.dart
@@ -1,4 +1,3 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import '../enums.dart';
 import '../grafik_element.dart';
 
@@ -42,62 +41,4 @@ class TimeIssueElement extends GrafikElement {
     closed: closed,
   );
 
-  @override
-  Map<String, dynamic> toJson() {
-    final base = baseToJson();
-    return {
-      ...base,
-      'issueType': issueType.toString(),
-      'issuePaymentType': issuePaymentType.toString(),
-      'workerId': workerId,
-      'taskId': taskId,
-      'fromTaskId': fromTaskId,
-      'toTaskId': toTaskId,
-      'makeupForIds': makeupForIds,
-      'isBalanced': isBalanced,
-    };
-  }
-
-  factory TimeIssueElement.fromJson(Map<String, dynamic> json) {
-    final id = json['id'] as String? ?? '';
-    final startTs = json['startDateTime'] as Timestamp?;
-    final endTs = json['endDateTime'] as Timestamp?;
-    final additionalInfo = json['additionalInfo'] as String? ?? '';
-    final addedByUserId = json['addedByUserId'] as String? ?? '';
-    final addedTimestamp = (json['addedTimestamp'] as Timestamp?)?.toDate() ?? DateTime(1960, 2, 9);
-    final closed = json['closed'] as bool? ?? false;
-
-    final issueTypeStr = json['issueType'] as String? ?? 'TimeIssueType.Nieobecnosc';
-    final issueType = TimeIssueType.values.firstWhere(
-          (e) => e.toString() == issueTypeStr,
-      orElse: () => TimeIssueType.Nieobecnosc,
-    );
-
-    final issuePaymentStr = json['issuePaymentType'] as String? ?? 'PaymentType.zero';
-    final issuePaymentType = PaymentType.values.firstWhere(
-          (e) => e.toString() == issuePaymentStr,
-      orElse: () => PaymentType.zero,
-    );
-
-    return TimeIssueElement(
-      id: id,
-      startDateTime: startTs?.toDate() ?? DateTime.now(),
-      endDateTime: endTs?.toDate() ?? DateTime.now(),
-      additionalInfo: additionalInfo,
-      issueType: TimeIssueType.values.firstWhere(
-            (e) => e.toString() == (json['issueType'] ?? 'TimeIssueType.Nieobecnosc'),
-        orElse: () => TimeIssueType.Nieobecnosc,
-      ),
-      issuePaymentType: issuePaymentType,
-      workerId: json['workerId'] as String? ?? '',
-      taskId: json['taskId'] as String?,
-      fromTaskId: json['fromTaskId'] as String?,
-      toTaskId: json['toTaskId'] as String?,
-      makeupForIds: (json['makeupForIds'] as List?)?.cast<String>(),
-      isBalanced: json['isBalanced'] as bool? ?? false,
-      addedByUserId: addedByUserId,
-      addedTimestamp: addedTimestamp,
-      closed: closed,
-    );
-  }
 }
