94d71453e4f80f045c53d26df2feff990bc17b37
JanushMaj <35202635+JanushMaj@users.noreply.github.com>
2025-07-11 13:10:23 +0200

    Add missing flutter_bloc import

--- feature/assign_employee/assign_employee_screen.dart
+++ feature/assign_employee/assign_employee_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import '../../theme/app_tokens.dart';
 import '../../shared/responsive/responsive_layout.dart';
 import '../../shared/form/custom_button.dart';
