f11521a410dba94ff624fcdd5f660d1caa1337a1
JanushMaj <35202635+JanushMaj@users.noreply.github.com>
2025-07-24 18:34:58 +0200

    Set task status closed by default

--- test/domain/grafik/supply_run_to_task_test.dart
+++ test/domain/grafik/supply_run_to_task_test.dart
@@ -30,7 +30,7 @@ void main() {
     expect(task.taskType, GrafikTaskType.Zaopatrzenie);
     expect(task.orderId, 'ord');
     expect(task.carIds, ['c1']);
-    expect(task.status, GrafikStatus.Realizacja);
+    expect(task.status, GrafikStatus.Zakonczone);
     expect(task.closed, isFalse);
   });
 }
